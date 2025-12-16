749352b2170166d24fd317b4b9924ad73e628507
AzPepoze <pladumguy@gmail.com>
2025-12-17 05:36:02 +0700

    remove base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/waifu-randomizer/' // Assuming the repo name is waifu-randomizer for GitHub Pages
 })
